69585c9f09f51478d191baee533bcb60a8e6470d
HubbardHuang <hubbard_huang@sina.com>
2019-01-30 08:32:24 +0800

    题目：旋转数组的最小数字
    URL: https://www.nowcoder.com/practice/9f3231a991af4f55b95579b44b7a01ba?tpId=13&tqId=11159&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking

---
+++ CodingInterviews/ci_06.cpp
@@ -0,0 +1,27 @@
+/*
+题目：旋转数组的最小数字
+
+题目描述：
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。
+例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。
+NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。
+*/
+
+class Solution {
+public:
+    int minNumberInRotateArray(vector<int> rotateArray) {
+        if (rotateArray.size() == 0) {
+            return 0;
+        }
+
+        int cur_pos = 0;
+        int next_pos = cur_pos + 1;
+        for (; next_pos < rotateArray.size(); next_pos++) {
+            if (rotateArray[cur_pos] > rotateArray[next_pos]) {
+                return rotateArray[next_pos];
+            }
+        }
+        return rotateArray[0];
+    }
+};
