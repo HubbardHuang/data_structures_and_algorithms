adc8f67c520ca8e0dbb7015dd6a33c2880742792
HubbardHuang <hubbard_huang@sina.com>
2019-01-31 16:22:53 +0800

    题目：二进制中1的个数
    URL: https://www.nowcoder.com/practice/8ee967e43c2c4ec193b040ea7fbb10b8?tpId=13&tqId=11164&tPage=1&rp=1&ru=%2Fta%2Fcoding-interviews&qru=%2Fta%2Fcoding-interviews%2Fquestion-ranking

---
+++ CodingInterviews/ci_11.cpp
@@ -0,0 +1,19 @@
+/*
+题目：二进制中1的个数
+
+题目描述：
+输入一个整数，输出该数二进制表示中1的个数。其中负数用补码表示。
+*/
+
+class Solution {
+public:
+    int NumberOf1(int n) {
+        int temp = n;
+        int count = 0;
+        while (temp) {
+            temp &= (temp - 1);
+            ++count;
+        }
+        return count;
+    }
+};
