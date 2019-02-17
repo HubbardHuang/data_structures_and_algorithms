3ebd75ff302951691357e0fed698d37fd77ad886
HubbardHuang <hubbard_huang@sina.com>
2019-02-17 10:36:50 +0800

    题目：对称的二叉树
    URL: https://www.nowcoder.com/practice/ff05d44dfdb04e1d83bdbdab320efbcb?tpId=13&tqId=11211&tPage=3&rp=3&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking

---
+++ CodingInterviews/ci_58.cpp
@@ -0,0 +1,42 @@
+/*
+题目：对称的二叉树
+
+题目描述：
+请实现一个函数，用来判断一颗二叉树是不是对称的。
+注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
+*/
+
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+    TreeNode(int x)
+      : val(x)
+      , left(NULL)
+      , right(NULL) {}
+};
+*/
+class Solution {
+public:
+    bool isSymmetrical(TreeNode* root) {
+        if (!root) {
+            return true;
+        }
+        return Compare(root->left, root->right);
+    }
+
+private:
+    bool Compare(TreeNode* left, TreeNode* right) {
+        if (!left && !right) {
+            return true;
+        }
+        if (!left || !right) {
+            return false;
+        }
+        if (left->val != right->val) {
+            return false;
+        }
+        return Compare(left->right, right->left) && Compare(left->left, right->right);
+    }
+};
