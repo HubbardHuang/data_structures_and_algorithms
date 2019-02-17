095f88a8e0108a47ab265a3c69bd79f24605b0f1
HubbardHuang <hubbard_huang@sina.com>
2019-02-17 17:23:09 +0800

    题目：序列化二叉树
    URL: https://www.nowcoder.com/practice/cf7e25aa97c04cc1a68c8f040e71fb84?tpId=13&tqId=11214&rp=3&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking

---
+++ CodingInterviews/ci_61.cpp
@@ -0,0 +1,56 @@
+/*
+题目：序列化二叉树
+
+题目描述：
+请实现两个函数，分别用来序列化和反序列化二叉树。
+*/
+
+struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+    TreeNode(int x)
+      : val(x)
+      , left(NULL)
+      , right(NULL) {}
+};
+
+class Solution {
+private:
+    int index = 0;
+
+public:
+    char* Serialize(TreeNode* node) {
+        if (!node) {
+            return "#,";
+        }
+        string curr = to_string(node->val);
+        curr.push_back(',');
+        char* left = Serialize(node->left);
+        char* right = Serialize(node->right);
+
+        int length = curr.size() + strlen(left) + strlen(right);
+        char* result = new char[length + 1];
+        result[length] = '\0';
+        strcpy(result, curr.data());
+        strcat(result, left);
+        strcat(result, right);
+        return result;
+    }
+    TreeNode* Deserialize(char* str) {
+        if (str[index] == '#') {
+            index += 2;
+            return nullptr;
+        }
+        int num = 0;
+        while (str[index] != ',' && str[index] != '\0') {
+            num = num * 10 + (str[index] - '0');
+            ++index;
+        }
+        ++index;
+        TreeNode* root = new TreeNode(num);
+        root->left = Deserialize(str);
+        root->right = Deserialize(str);
+        return root;
+    }
+};
