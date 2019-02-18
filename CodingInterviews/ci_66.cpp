f6d88183f23c101f5e736930bb8727d5539aaa1b
HubbardHuang <hubbard_huang@sina.com>
2019-02-18 17:26:25 +0800

    题目：机器人的运动范围
    URL: https://www.nowcoder.com/practice/6e5207314b5241fb83f2329e89fdecc8?tpId=13&tqId=11219&rp=3&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking

---
+++ CodingInterviews/ci_66.cpp
@@ -0,0 +1,53 @@
+/*
+题目：机器人的运动范围
+
+题目描述：
+地上有一个m行和n列的方格。
+一个机器人从坐标0,0的格子开始移动，
+每一次只能向左，右，上，下四个方向移动一格，
+但是不能进入行坐标和列坐标的数位之和大于k的格子。
+例如，当k为18时，机器人能够进入方格（35,37），因为3+5+3+7 = 18。
+但是，它不能进入方格（35,38），因为3+5+3+8 = 19。
+请问该机器人能够达到多少个格子？
+*/
+
+class Solution {
+private:
+    int GetBitSum(int num) {
+        int sum = 0;
+        while (num) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+    int CountRecursively(int curr_row, int curr_col, int threshold, bool** visited,
+                         int rows, int cols) {
+        if (0 <= curr_row && curr_row < rows && 0 <= curr_col && curr_col < cols &&
+            visited[curr_row][curr_col] == false &&
+            GetBitSum(curr_row) + GetBitSum(curr_col) <= threshold) {
+            visited[curr_row][curr_col] = true;
+            return 1 +
+                   CountRecursively(curr_row + 1, curr_col, threshold, visited, rows,
+                                    cols) +
+                   CountRecursively(curr_row - 1, curr_col, threshold, visited, rows,
+                                    cols) +
+                   CountRecursively(curr_row, curr_col + 1, threshold, visited, rows,
+                                    cols) +
+                   CountRecursively(curr_row, curr_col - 1, threshold, visited, rows,
+                                    cols);
+        }
+        return 0;
+    }
+
+public:
+    int movingCount(int threshold, int rows, int cols) {
+        bool** visited = new bool*[rows];
+        for (int i = 0; i < rows; i++) {
+            visited[i] = new bool[cols + 1];
+            fill(visited[i], visited[i] + cols, false);
+            visited[i][cols] = 0;
+        }
+        return CountRecursively(0, 0, threshold, visited, rows, cols);
+    }
+};
