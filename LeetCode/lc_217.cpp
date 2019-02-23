9a8b4dcf9b6f0b7e810b63fb3b25cc5c4f81ca24
HubbardHuang <hubbard_huang@sina.com>
2019-02-23 22:10:21 +0800

    Contains Duplicate
    URL: https://leetcode.com/problems/contains-duplicate/

---
+++ LeetCode/lc_217.cpp
@@ -0,0 +1,39 @@
+/*
+Title: Contains Duplicate
+
+Description:
+Given an array of integers,
+find if the array contains any duplicates.
+Your function should return true if any value appears at least twice in the array,
+and it should return false if every element is distinct.
+
+Example 1:
+Input: [1,2,3,1]
+Output: true
+
+Example 2:
+Input: [1,2,3,4]
+Output: false
+
+Example 3:
+Input: [1,1,1,3,3,4,3,2,4,2]
+Output: true
+*/
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int length = nums.size();
+        if (length == 0 || length == 1) {
+            return false;
+        }
+        unordered_map<int, int> count;
+        for (auto x : nums) {
+            count[x]++;
+            if (count[x] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
