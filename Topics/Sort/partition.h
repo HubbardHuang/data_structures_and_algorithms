dd29698cbc755b4b15bc4169c7b6368f995423fd
HubbardHuang <hubbard_huang@sina.com>
2019-04-01 00:12:17 +0800

    Remove needless precompiled code

--- Topics/Sort/partition.h
+++ Topics/Sort/partition.h
@@ -4,19 +4,6 @@
 #include "range.h"
 #include <cstddef>
 
-// If there is no bug, open this.
-#define RELEASE
-
-// If there is some errors that requires printing details, open it.
-// #define DEBUG
-
-// If you want to use some samples to test to find bugs, open it.
-// #define TEST
-
-#if (defined DEBUG || defined TEST)
-#define PRINT
-#endif
-
 Range<size_t> Partition(int nums[], size_t start_index, size_t end_index);
 
 #endif
