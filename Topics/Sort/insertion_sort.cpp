eb1ce063e540362ee396aedd2d42e057743bb751
HubbardHuang <hubbard_huang@sina.com>
2019-03-31 15:23:46 +0800

    Modify precompiled commands

--- Topics/Sort/insertion_sort.cpp
+++ Topics/Sort/insertion_sort.cpp
@@ -1,7 +1,31 @@
-#include "insertion_sort.h"
 #include <algorithm>
 #include <cstdio>
+#include <ctime>
+#include <vector>
 
+#include "insertion_sort.h"
+
+#ifdef RELEASE
+void
+InsertionSort(int nums[], size_t start_index, size_t end_index) {
+    if (start_index >= end_index) {
+        return;
+    }
+
+    for (size_t i = start_index + 1; i < end_index; i++) {
+        int inserted = nums[i];
+        size_t seat = i;
+        for (size_t k = i - 1; start_index <= k && k < end_index && inserted < nums[k];
+             k--) {
+            nums[k + 1] = nums[k];
+            seat = k; // update the seat where will be inserted the variable "inserted"
+        }
+        nums[seat] = inserted;
+    }
+}
+#endif
+
+#ifdef PRINT
 void
 Print(int nums[], int start_index, int end_index) {
     for (int i = start_index; i < end_index; i++) {
@@ -9,6 +33,9 @@ Print(int nums[], int start_index, int end_index) {
     }
     printf("\n");
 }
+#endif
+
+#ifdef PRINT
 void
 InsertionSort(int nums[], size_t start_index, size_t end_index) {
     if (start_index >= end_index) {
@@ -38,15 +65,61 @@ InsertionSort(int nums[], size_t start_index, size_t end_index) {
 #endif
     }
 }
+#endif
 
+#ifdef TEST
+size_t
+GetRandomNum(size_t min, size_t max) {
+    return rand() % (max - min + 1) + min;
+}
+#endif
+
+#ifdef TEST
+void
+TestInsertionSort(int sample_count) {
+    srand(static_cast<unsigned>(time(nullptr)));
+    for (int i = 0; i < sample_count; i++) {
+        // Generate a sample
+        std::vector<int> sample(GetRandomNum(0, 20));
+        for (decltype(sample.size()) i = 0; i < sample.size(); i++) {
+            sample[i] = GetRandomNum(0, 15);
+        }
+        printf("---------------------\n");
+        printf("sample:");
+        for (auto x : sample) {
+            printf(" %d", x);
+        }
+        printf("\n");
+
+        // This array will be used to be sorted by function "InsertionSort()"
+        int* tested_array = new int[sample.size()];
+        for (decltype(sample.size()) i = 0; i < sample.size(); i++) {
+            tested_array[i] = sample[i];
+        }
+
+        // This array will be sorted by function "std::sort()"
+        std::vector<int> answer = sample;
+
+        InsertionSort(tested_array, 0, sample.size());
+        std::sort(answer.begin(), answer.end());
+        bool no_error = true;
+        for (decltype(sample.size()) i = 0; i < sample.size(); i++) {
+            if (tested_array[i] != answer[i]) {
+                no_error = false;
+                break;
+            }
+        }
+        printf("Result: %s\n", no_error ? "correct" : "error");
+    }
+}
+#endif
+
+#ifdef PRINT
 int
 main(void) {
-    int nums[] = { 7, 20, 19, 1, 19, 15, 12, 16, 13, 1, 8, 11, 19, 15, 10, 4, 18, 9, 1 };
-    int arr[] = { 7, 20, 19, 1, 19, 15, 12, 16, 13, 1, 8, 11, 19, 15, 10, 4, 18, 9, 1 };
-    Print(nums, 0, 19);
-    InsertionSort(nums, 0, 19);
-    std::sort(arr, arr + 19);
-    Print(nums, 0, 19);
-    Print(arr, 0, 19);
+#ifdef TEST
+    TestInsertionSort(10);
+#endif
     return 0;
 }
+#endif
