d37c7f3e7740103dfe0e08d1a4fed397a10e0c6f
HubbardHuang <hubbard_huang@sina.com>
2019-04-28 16:15:09 +0800

    Reverse Integer
    URL: https://leetcode.com/problems/reverse-integer/

---
+++ LeetCode/lc_7.cpp
@@ -0,0 +1,38 @@
+/*
+Title: Reverse Integer
+
+Descripition:
+Given a 32-bit signed integer,
+reverse digits of an integer.
+
+Example 1:
+Input: 123
+Output: 321
+
+Example 2:
+Input: -123
+Output: -321
+
+Example 3:
+Input: 120
+Output: 21
+
+Note:
+Assume we are dealing with an environment
+which could only store integers within the 32-bit signed integer
+range: [−231,  231 − 1]. For the purpose of this problem,
+assume that your function returns 0 when the reversed integer overflows.
+*/
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long result = 0;
+        while (x) {
+            int tail = x % 10;
+            result = (long long)result * 10 + tail;
+            x /= 10;
+        }
+        return (result > INT_MAX || result < INT_MIN) ? 0 : result;
+    }
+};
