6daee4a1202091ff9caad672ed828ce1975b8748
HubbardHuang <hubbard_huang@sina.com>
2019-07-05 07:38:53 +0800

    Partition(2)
    Two way, opposite_direction.

--- Topics/Partition/2way_partition.cpp
+++ Topics/Partition/2way_partition.cpp
@@ -2,13 +2,14 @@
 #include <iostream>
 #include <vector>
 
-#define UNIDIRECTIONAL
+// #define SINGLE_DIRECT
+#define OPPOSITE_DIRECT
 
 using Type = int;
 
 void
 TwoWayPartition(std::vector<Type>& array) {
-#ifdef UNIDIRECTIONAL
+#ifdef SINGLE_DIRECT
     Type pivot = array[0];
     size_t left = 0;
     for (size_t right = 1; right < array.size(); right++) {
@@ -17,12 +18,30 @@ TwoWayPartition(std::vector<Type>& array) {
         }
     }
     std::swap(array[0], array[left]);
+#elif defined OPPOSITE_DIRECT
+    Type pivot = array[0];
+    size_t left = 0;
+    size_t right = array.size();
+    while (true) {
+        while (left < array.size() - 1 && array[++left] < pivot) {
+            ;
+        }
+        while (right >= 1 && array[--right] > pivot) {
+            ;
+        }
+        if (left < right) {
+            std::swap(array[left], array[right]);
+        } else {
+            break;
+        }
+    }
+    std::swap(array[right], array[0]);
 #endif
 }
 
 int
 main(void) {
-    std::vector<Type> array = { 9, 5, 9, 23, 54, 89, 9, 18 };
+    std::vector<Type> array = { 40, 9, 5, 9, 23, 54, 89, 9, 18 };
     TwoWayPartition(array);
     for (auto i : array) {
         std::cout << i << ' ';
